4215d82f9632442f74029a3a15527e249bb5eef6
David Cooper <dcooper@trapeze.com>
2011-08-09 02:27:11 -0400

    Updated sample_program.py to parse multiple files.

--- scripts/sample_program.py
+++ scripts/sample_program.py
@@ -30,26 +30,29 @@ sys.path.append(PROJECT_PATH)
 
 from fitparse.activity import Activity
 
-filename = None
+quiet = 'quiet' in sys.argv or '-q' in sys.argv
+filenames = None
 
-if len(sys.argv) >= 2 and os.path.exists(sys.argv[1]):
-    filename = sys.argv[1]
+if len(sys.argv) >= 2:
+    filenames = [f for f in sys.argv[1:] if os.path.exists(f)]
 
-if not filename:
-    filename = os.path.join(PROJECT_PATH, 'tests', 'data', 'sample-activity.fit')
+if not filenames:
+    filenames = [os.path.join(PROJECT_PATH, 'tests', 'data', 'sample-activity.fit')]
 
-a = Activity(filename)
-a.parse()
-
-
-def print_records(records):
-    for rec in records:
+def print_records(activity):
+    for rec in activity.records:
         print ("----- #%d: %s (%d entries) " % (rec.num, rec.type.name, len(rec.fields))).ljust(60, '-')
         for field in rec.fields:
             print "%s [%s]: %s " % (field.name, field.type.name, field.data)
+    print
 
-if 'quiet' not in sys.argv:
-    if '-a' not in sys.argv:
-        print_records(r for r in a.records if r.definition.type.num == 20)
+for f in filenames:
+    if quiet:
+        print f
     else:
-        print_records(a.records)
+        print ('##### %s ' % f).ljust(60, '#')
+
+    a = Activity(f)
+    a.parse()
+    if not quiet:
+        print_records(a)
