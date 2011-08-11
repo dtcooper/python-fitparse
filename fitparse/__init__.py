c5d78598d65058a82ef4e6424da660410a9a8568
David Cooper <dcooper@trapeze.com>
2011-08-11 17:16:53 -0400

    Added __all__ to __init__.py to pass pep8/pyflakes.

--- fitparse/__init__.py
+++ fitparse/__init__.py
@@ -22,3 +22,5 @@
 
 from fitparse.activity import Activity
 from fitparse.exceptions import FitError, FitParseError, FitParseComplete
+
+__all__ = ['Activity', 'FitError', 'FitParseError', 'FitParseComplete']
