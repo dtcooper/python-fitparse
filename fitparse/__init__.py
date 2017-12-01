ea481db61cede199b13d4d73038db8ed664bdb10
Carey Metcalfe <carey@cmetcalfe.ca>
2017-12-01 10:08:31 -0500

    Add JSON output option to fitdump script
    Other minor things:
    - Removed non-implemented output formats
    - Removed the restrction on dumping non-readable formats to stdout
    - Allow Python to buffer the output for faster execution
    
    Fixes #41

--- fitparse/__init__.py
+++ fitparse/__init__.py
@@ -1,9 +1,10 @@
 from fitparse.base import FitFile, FitParseError
+from fitparse.records import DataMessage
 from fitparse.processors import FitFileDataProcessor, StandardUnitsDataProcessor
 
 
 __version__ = '1.0.1'
 __all__ = [
     'FitFileDataProcessor', 'FitFile', 'FitParseError',
-    'StandardUnitsDataProcessor',
+    'StandardUnitsDataProcessor', 'DataMessage'
 ]
