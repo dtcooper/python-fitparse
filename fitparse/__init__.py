470ff2995c789b5bce3f7fab5bc15bf21848b810
Carey Metcalfe <carey@cmetcalfe.ca>
2019-02-27 11:47:40 -0500

    Bump version to v1.1.0

--- fitparse/__init__.py
+++ fitparse/__init__.py
@@ -3,7 +3,7 @@ from fitparse.records import DataMessage
 from fitparse.processors import FitFileDataProcessor, StandardUnitsDataProcessor
 
 
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 __all__ = [
     'FitFileDataProcessor', 'FitFile', 'FitParseError',
     'StandardUnitsDataProcessor', 'DataMessage'
