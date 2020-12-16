14f54f9bfdc9b448d0a9d62faa51811f5235255f
Carey Metcalfe <carey@cmetcalfe.ca>
2020-12-16 00:31:14 -0500

    Test warnings

--- tests/test.py
+++ tests/test.py
@@ -5,6 +5,7 @@ import datetime
 import os
 from struct import pack
 import sys
+import warnings
 
 from fitparse import FitFile
 from fitparse.processors import UTC_REFERENCE, StandardUnitsDataProcessor
@@ -413,7 +414,10 @@ class FitFileTestCase(unittest.TestCase):
 
     def test_mismatched_field_size(self):
         f = FitFile(testfile('coros-pace-2-cycling-misaligned-fields.fit'))
-        f.parse()
+        with warnings.catch_warnings(record=True) as w:
+            f.parse()
+            assert len(w) == 5
+            assert all("falling back to byte encoding" in str(x) for x in w)
         self.assertEqual(len(f.messages), 11293)
 
     # TODO:
